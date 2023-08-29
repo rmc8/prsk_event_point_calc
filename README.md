bddecb4a0dfca38f2cd08c02617f545f85840462
rmc8 <k@rmc-8.com>
2023-08-29 19:54:23 +0900

    calc

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # prsk_event_point_calc
+
+**\* English follows Japanese**
+
+このRepositoryでは、イベントポイントの計算結果を公開しています。
+
+## データの形式とアクセス方法
+- **tableディレクトリ** : TSV形式で計算結果を参照できます。
+- **apiディレクトリ** : JSON形式で計算結果を配信しています。
+
+## データの作成方法
+- `calc.py` : tableディレクトリ内のデータを作成します。
+- `create_json.py` : tableディレクトリのデータからapiディレクトリのデータを生成します。
+
+## ライブラリと基本ポイント
+- **libディレクトリ** : イベントポイントの計算用のライブラリと基本ポイントを記載したリストがあります。
+
+## テストについて
+- **testディレクトリ** : ライブラリの動作を外部ファイルを使って照合し、正しく動作することを検証しています。
+
+# prsk_event_point_calc \[English]
+
+This repository provides calculated results for event points.
+
+## Data Format and Access Method
+- **table directory**: The results can be referenced in TSV format.
+- **api directory**: The results are distributed in JSON format.
+
+## How to Generate Data
+- `calc.py`: Creates data within the table directory.
+- `create_json.py`: Generates data for the api directory from the data in the table directory.
+
+## Library and Basic Points
+- **lib directory**: Contains a library for event point calculations and a list detailing basic points.
+
+## About Testing
+- **test directory**: Validates the correct operation of the library by cross-referencing with external files.
--- calc.py
+++ calc.py
@@ -1,14 +1,10 @@
 from typing import List, Tuple
 from itertools import product
-from lib import event_point as ep
 
-BASIC_POINTS: List[int] = [
-    100, 103, 104, 105, 106, 107,
-    108, 109, 110, 111, 112, 113,
-    114, 115, 117, 117, 118, 119,
-    120, 121, 122, 123, 124, 125,
-    126, 127, 128, 130
-]
+import pandas as pd
+
+from lib import event_point as ep
+from lib.bp import BASIC_POINTS
 
 
 def get_score_ranges() -> List[Tuple[int, int]]:
@@ -16,10 +12,9 @@ def get_score_ranges() -> List[Tuple[int, int]]:
 
 
 def main():
-    import pandas as pd
-    df = pd.read_csv(r"test\data\test_data_bp100.tsv", sep="\t")
     for bp in BASIC_POINTS:
         prod = product(range(11), range(386), get_score_ranges())
+        table: list = []
         for lb, eb, (lower_limit, upper_limit) in prod:
             pt = ep.calc(
                 score=lower_limit,
@@ -27,11 +22,17 @@ def main():
                 basic_point=bp,
                 live_bonus=lb,
             )
-            if lb == 0 and bp == 100:
-                c = df[(df["LiveBonus"]==0)&(df["EventBonus"]==ep)&(df["ScoreLowerLimit"]==lower_limit)&(df["EventPoint"]==pt)]
-                if not c.empty:
-                    raise Exception("Err")
-        break
+            rec = {
+                "eventBonus": eb,
+                "liveBonus": lb,
+                "scoreLowerLimit": lower_limit,
+                "scoreUpperLimit": upper_limit,
+                "eventPoint": pt,
+            }
+            table.append(rec)
+        df = pd.DataFrame(table)
+        path = f"table/event_point_table_bp{bp}.tsv"
+        df.to_csv(path, sep="\t", index=False)
 
 
 if __name__ == "__main__":
